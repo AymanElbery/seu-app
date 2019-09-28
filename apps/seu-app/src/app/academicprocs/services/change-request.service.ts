704e66cc5b0da9ed7a5b937ca5758e0f9dba702e
Osama <O.Bisheer@ysoultions.net>
2019-09-28 17:05:23 +0200

    Change Request Branch

---
+++ apps/seu-app/src/app/academicprocs/services/change-request.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ConfigService } from 'src/app/shared/services/config.service';
+import { HttpRequestService } from 'src/app/shared/services/http-request.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChangeRequestService {
+
+  reqData;
+  msgs;
+  constructor(private configService: ConfigService, private httRequest: HttpRequestService) { }
+
+  getِgetRequests() {
+
+   return this.httRequest.GetRequest('change_camp_service?std_id=S120000101').toPromise();
+  }
+  AddRequest(data) {
+      return this.httRequest.postRequest('change_camp_service/insert?std_id=S120000101', data).toPromise();
+  }
+  deleteReq(id) {
+    return this.httRequest.GetRequest('change_camp_service/remove/' + id + '?std_id=S120000101').toPromise();
+
+  }
+
+   
+}
