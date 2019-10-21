050b01eb6a6425d9a45a28a92df9e821d6eecfbc
Mahmoud Hashem <m.hashem@ysolution.net>
2019-10-21 21:00:09 +0200

    add this.configService.baseUrl = "stdsUnivapi"; to all services ...

--- apps/seu-app/src/app/master-other-requests/services/graduate-profile.service.ts
+++ apps/seu-app/src/app/master-other-requests/services/graduate-profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ConfigService } from 'src/app/shared/services/config.service';
+import { HttpRequestService } from 'src/app/shared/services/http-request.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraduateProfileService {
 
-  constructor() { }
+  reqData;
+  msgs;
+  constructor(private configService: ConfigService, private httRequest: HttpRequestService) {
+    this.configService.baseUrl = "stdsUnivapi";
+  }
 }
