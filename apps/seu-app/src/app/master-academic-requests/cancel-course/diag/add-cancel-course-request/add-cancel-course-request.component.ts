826a44af951d896b72e79baa002fdecde526c76f
s.mohammed <s.mohammed@ysolution.net>
2019-11-03 16:56:41 +0200

    commit modifications

---
+++ apps/seu-app/src/app/master-academic-requests/cancel-course/diag/add-cancel-course-request/add-cancel-course-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cancel-course-request',
+  templateUrl: './add-cancel-course-request.component.html',
+  styleUrls: ['./add-cancel-course-request.component.css']
+})
+export class AddCancelCourseRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- apps/seu-app/src/app/master-academic-requests/services/cancel-course.service.ts
+++ apps/seu-app/src/app/master-academic-requests/services/cancel-course.service.ts
@@ -6,8 +6,20 @@ import { HttpRequestService } from 'src/app/shared/services/http-request.service
   providedIn: 'root'
 })
 export class CancelCourseService {
-
+  reqData;
+  msgs;
   constructor(private configService: ConfigService, private httRequest: HttpRequestService) {
     this.configService.baseUrl = 'stdsUnivapi';
+  }
+  getRequest() {
+    
+    return this.httRequest.GetRequest('course_cancel').toPromise();
+  }
+  AddRequest(data) {
+    return this.httRequest.postRequest('course_cancel/insert', data).toPromise();
+}
+  deleteReq(id) {
+    return this.httRequest.GetRequest('course_cancel/remove/' + id ).toPromise();
+
   }
 }
