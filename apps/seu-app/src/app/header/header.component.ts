302d68e8eeb6fe2a070734c8b11e657fca24f6f5
Moamen Mansour <m.mansour@mcit.gov.sa>
2020-01-02 18:43:32 +0300

    init ltr style

--- apps/seu-app/src/app/header/header.component.ts
+++ apps/seu-app/src/app/header/header.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserManagerService } from '../shared/services/user-manager.service';
-import { Subscription } from 'rxjs';
-import { UserServiceService } from '../shared/user-service.service';
-import { UserService } from '../account/services/user.service';
-import { NotificationsService } from '../shared/services/notificationsservice';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+// import { DOCUMENT } from "@angular/common";
+import { UserManagerService } from "../shared/services/user-manager.service";
+import { Subscription, from } from "rxjs";
+import { UserServiceService } from "../shared/user-service.service";
+import { UserService } from "../account/services/user.service";
+import { NotificationsService } from "../shared/services/notificationsservice";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  constructor(private userManager: UserManagerService, public userService: UserService, public notifications: NotificationsService, private translate: TranslateService) {
-
-
-  }
+  constructor(
+    private userManager: UserManagerService,
+    public userService: UserService,
+    public notifications: NotificationsService,
+    private translate: TranslateService
+  ) {}
   subscription: Subscription;
   isNavbarCollapsed;
   userData: any = {};
@@ -27,38 +30,53 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   logout() {
     this.userManager.logout();
-
   }
   useLang(code) {
     this.currLang = code;
     this.translate.use(code);
+    if (code === "en") {
+      this.loadExternalStyles("en-style.css")
+        .then(() => {
+          document.getElementById("html").setAttribute("lang", code);
+          document.getElementById("html").setAttribute("dir", "ltr");
+        })
+        .catch(() => {});
+    } else {
+      document.getElementById("html").setAttribute("lang", code);
+      document.getElementById("html").setAttribute("dir", "rtl");
+      document.getElementById("enStyle").remove();
+    }
   }
+
+  private loadExternalStyles(styleUrl: string) {
+    return new Promise((resolve, reject) => {
+      const styleElement = document.createElement("link");
+      styleElement.href = styleUrl;
+      styleElement.id = "enStyle";
+      styleElement.rel = "stylesheet";
+      styleElement.onload = resolve;
+      document.head.appendChild(styleElement);
+    });
+  }
+
   ngOnInit() {
     this.currLang = this.translate.currentLang;
     //console.log('header user data');
-    this.userService.userDataSubject.
-      subscribe(res => {
-        if (res) {
-          this.userData = this.userService.getActiveRoleDetails();
-          // if (!this.userService.userData.name_ar) {
-          //   setTimeout(() => {
-          //     window.location.reload();
-          //   }, 500);
-          // }
-        }
-      });
+    this.userService.userDataSubject.subscribe(res => {
+      if (res) {
+        this.userData = this.userService.getActiveRoleDetails();
+        // if (!this.userService.userData.name_ar) {
+        //   setTimeout(() => {
+        //     window.location.reload();
+        //   }, 500);
+        // }
+      }
+    });
 
     // // tslint:disable-next-line: triple-equals
     // if (this.userService.userData.name_ar === '') {
     //   setTimeout(() => {
     //     window.location.reload();
     //   }, 3000);
-
-
-
-
-
-
-
   }
 }
