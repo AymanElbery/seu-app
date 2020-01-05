19ce45aa7522d7688dfa116952fe207c95afaa66
alotfy <aalotfy@ysolution.net>
2020-01-05 21:23:37 +0200

    localization

--- apps/seu-app/src/app/finance-affair/finance-affair.module.ts
+++ apps/seu-app/src/app/finance-affair/finance-affair.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentPaymentComponent } from './student-payment/student-payment.component';
+import { AppLazyTransModule } from '../app.lazytrans.module';
 
 @NgModule({
   declarations: [TuitionFeesStatComponent,FeesExceptionComponent,AddFeesExceptionComponent, StudentPaymentComponent],
@@ -23,7 +24,8 @@ import { StudentPaymentComponent } from './student-payment/student-payment.compo
     FormsModule,
     MatDialogModule,
     MatProgressSpinnerModule,
-    ToastrModule.forRoot() 
+    ToastrModule.forRoot() ,
+    AppLazyTransModule
 
 
     
