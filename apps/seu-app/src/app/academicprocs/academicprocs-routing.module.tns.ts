e8bfb153069ee7f66b6af311318e5a179882894a
alotfy <aalotfy@ysolution.net>
2020-01-05 18:42:01 +0200

    mobile fix

--- apps/seu-app/src/app/academicprocs/academicprocs-routing.module.tns.ts
+++ apps/seu-app/src/app/academicprocs/academicprocs-routing.module.tns.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { WithdrawFromUnivComponent } from './withdraw-from-univ/withdraw-from-univ.component.tns';
 
-const routes: Routes = [
-  { path: '', component: WithdrawFromUnivComponent },
-  
-  {
-    path: 'procedures',
-    children: [
-      { path: 'withdrawuniv', component: WithdrawFromUnivComponent },
-    ]
-  }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
