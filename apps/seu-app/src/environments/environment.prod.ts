6abed3fca3cae68a775cd856bf428f406976b3de
alotfy <aalotfy@ysolution.net>
2020-01-12 14:48:26 +0200

    ar

--- apps/seu-app/src/environments/environment.prod.ts
+++ apps/seu-app/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  cmslink: "http://beta.seu.edu.sa/umbraco/Surface/",
-  baselink: "http://seuapps.seu.edu.sa/newservices/", //newservices/
-  ssolink: "http://seuapps.seu.edu.sa/newservices/sso/", //newservices/
-  servicesprefix: "api", //api
-  loginpage: "login-ss.php"
+  cmslink: 'http://beta.seu.edu.sa/umbraco/Surface/',
+  baselink: 'http://seuapps.seu.edu.sa/newservices/', // newservices/
+  ssolink: 'http://seuapps.seu.edu.sa/newservices/sso/', // newservices/
+  servicesprefix: 'api', // api
+  loginpage: 'login-ss.php'
 };
