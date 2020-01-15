d20d21f90c6a6a11f26c99571323209c09e7cdfe
alotfy <aalotfy@ysolution.net>
2020-01-15 15:51:27 +0200

    ADD WISHES FORM

--- apps/seu-app/src/app/shared/services/global.service.tns.ts
+++ apps/seu-app/src/app/shared/services/global.service.tns.ts
@@ -11,6 +11,6 @@ export class GlobalService extends GlobalBaseService {
   }
 
   getSID() {
-    return 'MWFmZTJmMTgtOTExZi00YmQ1LTg0ZTktZTYyY2VlNzdiOWQzfFNCdmF4L2JBSlA4Y3JRSnk5dUhxRlhaVGl4dz0=';  
-}
+    return 'MDljYzRhODAtOGM5Ni00ZjRjLTgyODItYzQ1YmUzODY4N2Y3fFNCdmF4L2JBSlA4Y3JRSnk5dUhxRlhaVGl4dz0=';
+  }
 }
