af74190fe22b907c5bbc23fc3356b882e01ef1e1
alotfy <aalotfy@ysolution.net>
2020-02-01 14:57:29 +0200

    start master

--- apps/seu-app/src/app/master-student-data/master-student-data-routing.module.tns.ts
+++ apps/seu-app/src/app/master-student-data/master-student-data-routing.module.tns.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { AcademicStatusComponent } from './academic-status/academic-status.component.tns';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'masteraff', component: AcademicStatusComponent },
+{
+  path: '',
+  children: [
+  { path: 'academicstatus',  component: AcademicStatusComponent }
+ // { path: 'academicrecord',  component: AcademicRecordComponent }
+
+ ]
+}
+
+];
 
 @NgModule({
   imports: [NativeScriptRouterModule.forChild(routes)],
