816975bbd68b848de5546f0a9ed50b1e5cda3986
amohamed <arwaali467@gmail.com>
2020-02-02 15:57:21 +0200

    -fix absolute paths -fix parse error

--- apps/seu-app/src/app/master-print-certificates/master-print-certificates.module.tns.ts
+++ apps/seu-app/src/app/master-print-certificates/master-print-certificates.module.tns.ts
@@ -9,9 +9,11 @@ import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular/listview-directives';
 import { NativeScriptLocalizeModule } from 'nativescript-localize/angular';
 import { AppTransModule } from '../app.trans.module';
+import { ExamAttendanceCertificateAppComponent } from './exam-attendance-certificate-app/exam-attendance-certificate-app.component';
 
 @NgModule({
-  declarations: [CertificateIDComponent,ExamAttendanceCertificateComponent],
+  declarations: [CertificateIDComponent,ExamAttendanceCertificateComponent,
+    ExamAttendanceCertificateAppComponent],
   imports: [
     MasterPrintCertificatesRoutingModule,
     NativeScriptCommonModule,
