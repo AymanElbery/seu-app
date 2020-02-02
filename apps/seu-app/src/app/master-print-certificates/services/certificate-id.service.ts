79e7465b828fa2b485953fd9801b596c442d4d7f
alotfy <aalotfy@ysolution.net>
2020-02-02 15:28:38 +0200

    push

--- apps/seu-app/src/app/master-print-certificates/services/certificate-id.service.ts
+++ apps/seu-app/src/app/master-print-certificates/services/certificate-id.service.ts
@@ -13,23 +13,23 @@ export class CertificateIDService {
     this.configService.baseUrl = 'stdsUnivapi';
   }
   getRequest() {
-    this.configService.baseUrl = "stdsUnivapi";
+    this.configService.baseUrl = 'stdsUnivapi';
 
     // ?std_id=S180105049
     return this.httRequest.GetRequest('identification_certificate').toPromise();
   }
   Download() {
 
-    this.configService.baseUrl = "stdsUnivapi";
+    this.configService.baseUrl = 'stdsUnivapi';
     const sid =   this.configService.getSid();
 
-    return this.configService.getApiURI() + '/identification_certificate/download?sid='+sid;
+    return this.configService.getApiURI() + '/identification_certificate/download?sid=' + sid;
    }
    DownloadEng() {
-    this.configService.baseUrl = "stdsUnivapi";
+    this.configService.baseUrl = 'stdsUnivapi';
     const sid =   this.configService.getSid();
 
 
-    return this.configService.getApiURI() + '/identification_certificate/download?Lang=en&sid='+sid;
+    return this.configService.getApiURI() + '/identification_certificate/download?Lang=en&sid=' + sid;
    }
 }
