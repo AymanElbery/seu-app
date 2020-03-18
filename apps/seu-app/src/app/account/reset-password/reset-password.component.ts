2d6c3df175e9265031937aad41e94309677b66cb
alotfy <aalotfy@ysolution.net>
2020-03-18 23:51:04 +0200

    reset passord mobile

--- apps/seu-app/src/app/account/reset-password/reset-password.component.ts
+++ apps/seu-app/src/app/account/reset-password/reset-password.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
 import { UserService } from '../services/user.service';
 import { AppToasterService } from 'src/app/shared/services/app-toaster';
 import { reduce } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,33 +11,34 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
-  reset: any;
+
 
   constructor(
     public userService: UserService,
     private toastr: AppToasterService,
-    private translate:TranslateService,
+    private translate: TranslateService,
     public dialogRef: MatDialogRef<ResetPasswordComponent>
   ) {
     this.reset = {
-      opassword: "",
-      npassword: "",
-      cpassword: ""
-    }
+      opassword: '',
+      npassword: '',
+      cpassword: ''
+    };
 
   }
+  reset: any;
+  requesting = false;
   ngOnInit() {
 
   }
-  requesting = false;
   addRequest(data: any) {
     this.userService.resetPassword(this.reset.opassword, this.reset.npassword, this.reset.cpassword).then(res => {
-      if (res['status']) {
-        this.toastr.push([{ type: 'scuccess', body: this.translate.instant("general.restsuccess") }]);
+      if ((res as any).status) {
+        this.toastr.push([{ type: 'scuccess', body: this.translate.instant('general.restsuccess') }]);
 
         this.dialogRef.close();
       } else {
-        this.toastr.push([{ type: 'error', body: res['error'] }]);
+        this.toastr.push([{ type: 'error', body: (res as any).error }]);
       }
       this.requesting = false;
     },
