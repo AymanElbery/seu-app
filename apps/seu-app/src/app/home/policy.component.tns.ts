9484421d9dc076807ed8ce91d8ec7ada77ebd3f2
alotfy <aalotfy@ysolution.net>
2020-04-17 12:15:26 +0200

    menu and privacy

---
+++ apps/seu-app/src/app/home/policy.component.tns.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-policy-home',
+  styleUrls: ['./policy.component.scss'],
+  templateUrl: './policy.component.html'
+})
+export class PolicyComponent   {
+ 
+  constructor(  ) {
+
+  }
+ 
+}
