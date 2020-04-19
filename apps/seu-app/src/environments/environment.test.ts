047d1d8ba5da0bbcc2afeb2cdaaa353f865dbeec
a.farouk <a.farouk@cpt-it.com>
2020-04-19 12:15:19 +0300

    update environments

--- apps/seu-app/src/environments/environment.test.ts
+++ apps/seu-app/src/environments/environment.test.ts
@@ -5,11 +5,11 @@ export const environment = {
   ssolink: 'https://seuapps.seu.edu.sa/newservices/sso/', // newservices/
   service_json: 'https://seuapps.seu.edu.sa/newservices/api/docs/ServicesInstructions/', //doenlaod and play vedios
   wafi_apilink: 'https://sso.seu.edu.sa/CPTIT_SEU_WS/jersey/', // wafi
-  servie_admission_UG: 'https://apps.seu.edu.sa/newservices/api/admission/ug',
-  paymentLink: "https://apps.seu.edu.sa/newservices/api/fees",
-  servicesprefix: 'api', // api
-  appsprefix: 'apps-prod', // api-test
-  loginpage: 'login-ss.php',
+  servie_admission_UG: 'https://apps.seu.edu.sa/newservices/api-test/admission/ug',
+  paymentLink: "https://apps.seu.edu.sa/newservices/api-test/fees",
+  servicesprefix: 'api-test', // api
+  appsprefix: 'apps-test', // api-test
+  loginpage: 'login-sst.php',
   notesAuth: 'nots:N0t!fic@ti0n$',
   ssoAuth: 'sso:s$0$3u2030',
   allowWafi: true,
