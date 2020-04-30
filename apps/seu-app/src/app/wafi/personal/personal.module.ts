98aec3bf7d094bc8a3ce33c79b9d8de12df6a9ea
vazid ali <coolvazidali@gmail.com>
2020-04-30 16:14:25 +0300

    updateinfo

--- apps/seu-app/src/app/wafi/personal/personal.module.ts
+++ apps/seu-app/src/app/wafi/personal/personal.module.ts
@@ -11,16 +11,27 @@ import { MatProgressSpinnerModule } from '@angular/material';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AppLazyTransModule } from 'src/app/app.lazytrans.module';
 import{SeucommonModule} from '../../seucommon/seucommon.module';
+import { ManagerialDutiesComponent } from './managerial-duties/managerial-duties.component';
+import { UpdateinfoComponent } from './personalinfo/updateinfo/updateinfo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 @NgModule({
-  declarations: [PersonalinfoComponent,SalariesComponent,SalarydetailComponent,VacationsComponent,PrintReportComponent],
-  entryComponents: [SalarydetailComponent],
+  declarations: [PersonalinfoComponent,SalariesComponent,SalarydetailComponent,VacationsComponent,
+    PrintReportComponent,ManagerialDutiesComponent,UpdateinfoComponent],
+  entryComponents: [SalarydetailComponent,UpdateinfoComponent],
   imports: [
     CommonModule,    
     PersonalRoutingModule,
     MatDialogModule,
     MatProgressSpinnerModule,
     AppLazyTransModule,
-    SeucommonModule
+    SeucommonModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    ReactiveFormsModule
   
   ]
 })
