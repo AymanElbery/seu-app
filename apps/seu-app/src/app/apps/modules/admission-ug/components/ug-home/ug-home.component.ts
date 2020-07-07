58b39cbf06db4047642f22be22a158ba6ea8ad4c
Farouk <a.farouk@cpt-it.com>
2020-07-07 15:35:53 +0300

    fix add new req on admission ug home

--- apps/seu-app/src/app/apps/modules/admission-ug/components/ug-home/ug-home.component.ts
+++ apps/seu-app/src/app/apps/modules/admission-ug/components/ug-home/ug-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdmissionUGService } from '../../services/admission-ug.service';
+import { TranslateService } from '@ngx-translate/core';
+import { AppToasterService } from 'src/app/shared/services/app-toaster';
 
 @Component({
   selector: 'app-ug-home',
@@ -8,7 +10,7 @@ import { AdmissionUGService } from '../../services/admission-ug.service';
 })
 export class UGHomeComponent implements OnInit {
   newRequest = false;
-  constructor(public admissionugService: AdmissionUGService) { }
+  constructor(public admissionugService: AdmissionUGService,private appToasterService: AppToasterService,private translate: TranslateService) { }
 
   ngOnInit() {
     this.checkForNewReqs();
@@ -22,6 +24,13 @@ export class UGHomeComponent implements OnInit {
       return;
     }
     this.newRequest = (this.admissionugService.settings['ADMISSION_NEWREQ'] == 1);
+  }
+  addNewReq() {
+    if (this.newRequest) {
+      return true;
+    }
+    this.appToasterService.push([{ type: 'error', body: this.translate.instant('service_closed') }]);
+    return false;
   }
 
 }
