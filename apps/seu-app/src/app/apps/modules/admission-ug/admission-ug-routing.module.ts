f7d8829cd3ee904d8f3da9170bfa810fd2e23c9d
Farouk <a.farouk@cpt-it.com>
2020-08-06 17:20:35 +0300

    fix ug component

--- apps/seu-app/src/app/apps/modules/admission-ug/admission-ug-routing.module.ts
+++ apps/seu-app/src/app/apps/modules/admission-ug/admission-ug-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UGHomeComponent } from './components/ug-home/ug-home.component';
 import { AdmissionResultComponent } from './components/admission-result/check/admission-result.component';
-import { PayFeeComponent } from './components/pay-fee/pay-fee.component';
+import { UGPayFeeComponent } from './components/pay-fee/pay-fee.component';
 import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
 import { DisplayResultComponent } from './components/admission-result/result/display-result.component';
 import { UgLoginComponent } from './components/ug-login/login/ug-login.component';
@@ -24,7 +24,7 @@ const routes: Routes = [
       { path: 'ug-verification', component: UgVeryficationComponent },
       { path: 'admission-result', component: AdmissionResultComponent, canActivate: [AppsUgGuard] },
       { path: 'display-result', component: DisplayResultComponent },
-      { path: 'pay-fee', component: PayFeeComponent, canActivate: [AppsUgGuard] },
+      { path: 'pay-fee', component: UGPayFeeComponent, canActivate: [AppsUgGuard] },
       { path: 'pay-callback/:tid', component: PayFeeCheckComponent},
       { path: 'match-documents', component: MatchDocumentsComponent, canActivate: [AppsUgGuard] },
       { path: 'upload-documents', component: UploadDocumentComponent, canActivate: [AppsUgGuard] },
