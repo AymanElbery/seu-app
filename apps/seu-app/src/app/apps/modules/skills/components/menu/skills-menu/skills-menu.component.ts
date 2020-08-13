cee8a1055c43a61f88d2935b7e71a99747d83b70
Farouk <a.farouk@cpt-it.com>
2020-08-13 12:15:13 +0300

    update menu

--- apps/seu-app/src/app/apps/modules/skills/components/menu/skills-menu/skills-menu.component.ts
+++ apps/seu-app/src/app/apps/modules/skills/components/menu/skills-menu/skills-menu.component.ts
@@ -5,11 +5,22 @@ import { Component, OnInit, AfterContentInit } from '@angular/core';
   templateUrl: './skills-menu.component.html',
   styleUrls: ['./skills-menu.component.css']
 })
-export class SkillsMenuComponent implements OnInit , AfterContentInit{
+export class SkillsMenuComponent implements OnInit, AfterContentInit {
 
   constructor() { }
-
+  menuType = '';
   ngOnInit() {
+
+    const currURL = document.location.href;
+    if (currURL.indexOf("/skills/emp") !== -1) {
+      this.menuType = 'emp';
+    } else if (currURL.indexOf("/std/emp") !== -1) {
+      this.menuType = 'std';
+    } else if (currURL.indexOf("/skills/staff") !== -1) {
+      this.menuType = 'staff';
+    }
+
+
   }
 
   ngAfterContentInit() {
