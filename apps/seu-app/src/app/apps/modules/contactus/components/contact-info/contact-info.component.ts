e5dcf4b117dc611c18d13186bcacd673c18b6c42
saleh <9oyle7@gmail.com>
2021-01-04 14:13:43 +0300

    update styles & content

--- apps/seu-app/src/app/apps/modules/contactus/components/contact-info/contact-info.component.ts
+++ apps/seu-app/src/app/apps/modules/contactus/components/contact-info/contact-info.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-info',
   templateUrl: './contact-info.component.html',
-  styleUrls: ['./contact-info.component.css']
+  styleUrls: ['./contact-info.component.css','../styles/contact.css']
 })
 export class ContactInfoComponent implements OnInit {
 
--- apps/seu-app/src/app/apps/modules/contactus/components/contact-page/contact-page.component.ts
+++ apps/seu-app/src/app/apps/modules/contactus/components/contact-page/contact-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.css','../styles/contact.css']
 })
 export class ContactPageComponent implements OnInit {
 
