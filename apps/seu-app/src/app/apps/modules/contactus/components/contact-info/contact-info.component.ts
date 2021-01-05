97dc9bcc58718be6a628aed5d8a37351db5fc4af
Ahmed Farouk <ahmed4.farouk@gmail.com>
2021-01-05 10:58:56 +0300

    add branches and apply function

--- apps/seu-app/src/app/apps/modules/contactus/components/contact-info/contact-info.component.ts
+++ apps/seu-app/src/app/apps/modules/contactus/components/contact-info/contact-info.component.ts
@@ -1,15 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-info',
   templateUrl: './contact-info.component.html',
-  styleUrls: ['./contact-info.component.css','../styles/contact.css']
+  styleUrls: ['./contact-info.component.css', '../styles/contact.css']
 })
 export class ContactInfoComponent implements OnInit {
 
-  constructor() { }
+  selectedbranch;
+  branchesData;
+  branchesList = [];
+  loading = true;
+  constructor(private http: HttpClient, private dom: DomSanitizer) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.http.get("/assets/branches.json").subscribe((branches) => {
+      this.branchesData = branches;
+      let blist = [];
+      Object.keys(branches).forEach(key => {
+        blist.push({ code: key, title: branches[key]['title'] });
+        branches[key].maps.map(item => {
+          item['url'] = this.dom.bypassSecurityTrustResourceUrl(item['url']);
+          return item;
+        });
+      });
+      this.branchesList = blist;
+      this.selectedbranch = 'madina';
+      this.loading = false;
+    })
   }
 
 }
