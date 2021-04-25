f1a2bb2d784dda2a3d5bf661204727aebbf438dc
saleh <9oyle7@gmail.com>
2021-04-25 14:00:42 +0300

    new ID

--- apps/seu-app/src/environments/environment.ts
+++ apps/seu-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production:false,
-  cmslink: 'https://test.seu.edu.sa/umbraco/Surface/',
+  cmslink: 'https://seu.edu.sa/umbraco/Surface/',
   baselink: 'https://seuapps.seu.edu.sa/newservices/', // newservices/
   ssolink: 'https://seuapps.seu.edu.sa/newservices/sso', // newservices/
   service_json: 'https://seuapps.seu.edu.sa/newservices/api/docs/ServicesInstructions/', //doenlaod and play vedios
