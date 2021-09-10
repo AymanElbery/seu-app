acf6af73906292bb7be0a6326f7c5b2ece0c5b30
AymanElbery <ayman.bery@gmail.com>
2021-09-10 03:09:46 +0300

    env test

--- apps/seu-app/src/environments/environment.test.ts
+++ apps/seu-app/src/environments/environment.test.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   cmslink: 'https://seu.edu.sa/umbraco/Surface/',
   baselink: 'https://seuapps.seu.edu.sa/newservices/', // newservices/
+  diplome_base_api_url: 'http://localhost:8000/api/', // newservices/
   ssolink: 'https://seuapps.seu.edu.sa/newservices/sso', // newservices/
   service_json: 'https://seuapps.seu.edu.sa/newservices/api/docs/ServicesInstructions/', //doenlaod and play vedios
   wafi_apilink: 'https://sso.seu.edu.sa/CPTIT_SEU_WS_TEST/jersey/', // wafi
