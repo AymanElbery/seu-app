b51eb2cc9d2032aa1c25e4d291b9a650542111e8
AymanElbery <ayman.bery@gmail.com>
2022-07-31 15:49:33 +0300

    doc_gr

--- apps/seu-app/src/app/apps/modules/stds-services/components/services-home/services-home.component.ts
+++ apps/seu-app/src/app/apps/modules/stds-services/components/services-home/services-home.component.ts
@@ -10,16 +10,20 @@ export class ServicesHomeComponent implements OnInit {
 
   isLoading = false;
   have_docs_permession = false;
+  have_docs_gr_permession = false;
   have_reenroll_permession = false;
   have_refund_permession = false;
   constructor(private stdservice: StudentService) { }
 
   ngOnInit() {
     this.have_docs_permession = this.stdservice.have_docs_permession;
+    this.have_docs_gr_permession = this.stdservice.have_docs_gr_permession;
+    console.log(this.have_docs_gr_permession);
     this.have_reenroll_permession = this.stdservice.have_reenroll_permession;
     this.have_refund_permession = this.stdservice.have_refund_permession;
     this.stdservice.userLoadedObservable.subscribe(() => {
       this.have_docs_permession = this.stdservice.have_docs_permession;
+      this.have_docs_gr_permession = this.stdservice.have_docs_gr_permession;
       this.have_reenroll_permession = this.stdservice.have_reenroll_permession;
       this.have_refund_permession = this.stdservice.have_refund_permession;
     });
